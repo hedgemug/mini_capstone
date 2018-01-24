e2c5744c907c4aafed3dc3e2136acb631fbb4b06
Dani Zaghian <dani.zaghian@gmail.com>
2018-01-23 19:43:25 -0800

    added validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,11 +6,11 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-                          name: params[:name],
-                          description: params[:description],
-                          price: params[:price],
-                          image_url: params[:image_url]
-                          )
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image_url: params[:image_url]
+    )
     product.save
     render json: product.as_json
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,30 @@
 class Product < ApplicationRecord
+
+  validates :name, presence: true
+
+  def is_discounted?
+    price.to_f <= 2
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      image_url: image_url,
+      price: price,
+      description: description,
+      tax: tax,
+      total: total,
+      is_discounted: is_discounted?
+    }
+  end
+
 end
